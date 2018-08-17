c5c83ad91c56fe0caf1981dc15c44c6b87149f34
Matt Loberg <loberg.matt@gmail.com>
2018-08-17 17:21:29 -0500

    add Laravel Passport Client UUIDs post

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ require('../css/app.css');
 // Include images
 require('../images/automate-all-the-things.jpg');
 require('../images/hero.jpg');
+require('../images/hero-i6VBVfcerso.jpg');
 require('../images/hero-znT2Mwt9ypo.jpg');
 require('../images/icon.png');
 require('../images/profile.jpg');
